0e5d5d1f6d059dcd459c165fcab823f321782aca
Marlon <perezmarlon538@gmail.com>
2020-07-13 13:55:40 -0600

    docs agregado

---
+++ docs/precache-manifest.6a7f80b9f43503e0bb207d407d5700bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c5f2ecc46021fe84ea7bc555658e36",
+    "url": "./index.html"
+  },
+  {
+    "revision": "458f98ef214c46ca8979",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cbf9e59743c0c9961410",
+    "url": "./static/css/main.2f6c386b.chunk.css"
+  },
+  {
+    "revision": "458f98ef214c46ca8979",
+    "url": "./static/js/2.170b1a91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.170b1a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf9e59743c0c9961410",
+    "url": "./static/js/main.603f354d.chunk.js"
+  },
+  {
+    "revision": "ce833ee9c99ed1a9adbf",
+    "url": "./static/js/runtime-main.0ceb1455.js"
+  }
+]);
